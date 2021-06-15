e60114eecb4729903075e01102f3bd0095299c91
JC Oettle <u18000135@tuks.co.za>
2021-06-15 20:15:41 +0200

    Random time seed en digits specify

--- jacobus/PRNG_BBS.py
+++ jacobus/PRNG_BBS.py
@@ -3,109 +3,85 @@
 # Permission granted by Gustan Naude
 import time
 
-# Number of digits, thus between 10^(numDigits-1) to (10^numDigits)-1
-numDigits = 6
+class PRNG_BBS:
+    def __init__(self):
+        
+        # Number of digits, thus between 10^(numDigits-1) to (10^numDigits)-1
+        # Default case is 5 digits
+        self.numDigits = 5
 
-rndMax = 10**(numDigits-1)
-rndMin = (10**numDigits)-1
+        # two large prime numbers, that also equals 3 when modded with 4
+        self.p = 1000000123
+        self.q = 1000000223
 
-# Euler totient function for prime numbers
-def ETF(a,b): 
-    return (a-1)*(b-1)
+        self.n = self.p*self.q
 
-# get GCD
-def extended_euclidean_algo(a,b):
-    # Base Case 
-    if a == 0 :  
-        return b,0,1
+        # seed that is relative prime to n
+        self.s = self.getSeed(self.p,self.q)
 
-    gcd,x1,y1 = extended_euclidean_algo(b%a, a) 
+    # Euler totient function for prime numbers
+    def ETF(self,a,b): 
+        return (a-1)*(b-1)
+
+    # get GCD
+    def extended_euclidean_algo(self,a,b):
+        # Base Case 
+        if a == 0 :  
+            return b,0,1
+
+        gcd,x1,y1 = self.extended_euclidean_algo(b%a, a) 
+        
+        # Update x and y using results of recursive 
+        # call 
+        x = y1 - (b//a) * x1 
+        y = x1 
+        
+        return gcd,x,y
+
+    def getSeed(self,p,q):
+        
+        # Max value the seed can be
+        max = self.ETF(p,q)
+
+        # Seed must not be too small
+        min = max // 2
+
+        while True:
+            testS = ((round(time.time()*1000)**3) % (max-min+1)) + min
+            if self.extended_euclidean_algo(testS,max)[0] == 1:
+                return testS
     
-    # Update x and y using results of recursive 
-    # call 
-    x = y1 - (b//a) * x1 
-    y = x1 
-    
-    return gcd,x,y
+    def getRandomNumber(self, digits=None):
 
-# get largest value that is coprime
-def getE(max,p,q):
-    
-    for j in range(max,-1,-1):
-        if extended_euclidean_algo(j,ETF(p,q))[0] == 1:
-            return j
+        if digits is not None:
+            self.numDigits = digits
 
-# two large prime numbers, that also equals 3 when modded with 4
-p = 1000000123
-q = 1000000223
+        # Generate ceil(log2((10^numDigits)-1)) bits and then scale to the 10^(numDigits-1) to (10^numDigits)-1 range using
+        # (random % (max-min+1))+min
 
-n = p*q
+        # Get number of bits by checking the amount of bits in (10^numDigits)-1 
+        numBits = len(bin(10**self.numDigits-1)[2:])
 
-# relative prime to n
-s = getE(ETF(p,q),p,q)
+        rndMax = 10**(self.numDigits-1)
+        rndMin = (10**self.numDigits)-1
 
-##### !!!!!! op die oomblik bly die seed dieselfed, kyk of jy time mag gebruik om seed te randomize
+        x = []
+        b = []
+        x.append(self.s**2 % self.n)
 
-# Generate ceil(log2((10^numDigits)-1)) bits and then scale to the 10^(numDigits-1) to (10^numDigits)-1 range using
-# (random % (max-min))+min
+        bits = ""
+        for i in range(numBits):
+            x.append(x[len(x)-1]**2 % self.n)
+            b.append(x[len(x)-1] % 2)
+            bits += str(b[len(b)-1])
 
-# Get number of bits by checking the amount of bits in (10^numDigits)-1 
-numBits = len(bin(10**numDigits-1)[2:])
+        rndNumber = (int(bits,2) % (rndMax-rndMin+1)) + rndMin
+
+        return rndNumber
 
 
-x = []
-b = []
-x.append(s**2 % n)
-bits = ""
-for i in range(numBits):
-    x.append(x[len(x)-1]**2 % n)
-    b.append(x[len(x)-1] % 2)
-    bits += str(b[len(b)-1])
+# Testing
+# BBS = PRNG_BBS()
 
-rndNumber = (int(bits,2) % (rndMax-rndMin)) + rndMin
-
-print(rndNumber)
-
-
-
-
-
-# bits = ""
-# for i in range(numBits):
-#     x.append(x[len(x)-1]**2 % n)
-#     b.append(x[len(x)-1] % 2)
-#     bits += str(b[len(b)-1])
-
-# rndNumber = (int(bits,2) % (rndMax-rndMin)) + rndMin
-
-# print(rndNumber)
-
-# bits = ""
-# for i in range(numBits):
-#     x.append(x[len(x)-1]**2 % n)
-#     b.append(x[len(x)-1] % 2)
-#     bits += str(b[len(b)-1])
-
-# rndNumber = (int(bits,2) % (rndMax-rndMin)) + rndMin
-
-# print(rndNumber)
-
-# bits = ""
-# for i in range(numBits):
-#     x.append(x[len(x)-1]**2 % n)
-#     b.append(x[len(x)-1] % 2)
-#     bits += str(b[len(b)-1])
-
-# rndNumber = (int(bits,2) % (rndMax-rndMin)) + rndMin
-
-# print(rndNumber)
-
-# bits = ""
-# for i in range(numBits):
-#     x.append(x[len(x)-1]**2 % n)
-#     b.append(x[len(x)-1] % 2)
-#     bits += str(b[len(b)-1])
-
-# rndNumber = (int(bits,2) % (rndMax-rndMin)) + rndMin
-
-# print(rndNumber)
+# for i in range(20):
+#     print(BBS.getRandomNumber(i+1))
